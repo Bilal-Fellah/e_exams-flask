69331c84fc85046990499f7bccd5e81e1eb96008
Bilal-Fellah <121223453+Bilal-Fellah@users.noreply.github.com>
2024-12-25 22:52:24 +0100

    fixed the credentials

--- api/routes/connection.py
+++ api/routes/connection.py
@@ -1,6 +1,9 @@
 import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url: str = os.environ.get("https://jqfljolmdgdpisbmmmgn.supabase.co")
-key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZmxqb2xtZGdkcGlzYm1tbWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxNDIwMDYsImV4cCI6MjA1MDcxODAwNn0.B9L-ou-5OFxnN8kMiO0Cu1gyKYpx6rPipE5mDrimZW4")
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
