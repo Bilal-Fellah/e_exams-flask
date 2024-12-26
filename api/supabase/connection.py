fc64609db53aef4a4b527fdc62d0044a32ed73d4
Bilal-Fellah <121223453+Bilal-Fellah@users.noreply.github.com>
2024-12-26 18:40:58 +0100

    check

--- api/supabase/connection.py
+++ api/supabase/connection.py
@@ -7,3 +7,6 @@ from supabase import create_client, Client
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+
+# this is just ro check
