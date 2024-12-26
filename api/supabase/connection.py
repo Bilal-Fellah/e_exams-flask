6453271e00eb91547476eb8f1fe6ad29e8f20bc8
Bilal-Fellah <121223453+Bilal-Fellah@users.noreply.github.com>
2024-12-26 16:18:16 +0100

    no use of dotenv

--- api/supabase/connection.py
+++ api/supabase/connection.py
@@ -1,9 +1,9 @@
 import os
 from supabase import create_client, Client
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
